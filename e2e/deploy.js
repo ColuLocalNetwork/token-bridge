1f3721b400745dccd348e6d8314f8f4ea3512292
Lior Rabin <lior@colu.com>
2019-01-16 11:12:12 +0200

    start implementing e2e tests

--- e2e/deploy.js
+++ e2e/deploy.js
@@ -16,6 +16,12 @@ shell.cp(path.join(envsDir, 'erc-contracts-deploy.env'), path.join(deployContrac
 shell.exec('node deploy.js')
 shell.rm('.env')
 shell.cp(
+  path.join(envsDir, 'erc-multiple-contracts-deploy.env'),
+  path.join(deployContractsDir, '.env')
+)
+shell.exec('node deploy.js')
+shell.rm('.env')
+shell.cp(
   path.join(envsDir, 'erc-native-contracts-deploy.env'),
   path.join(deployContractsDir, '.env')
 )
